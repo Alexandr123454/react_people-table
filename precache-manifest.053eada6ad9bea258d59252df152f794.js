9d0716385363cf21b360d4cce376e176e68c20f9
alexandr <alexandrov6927@gmail.com>
2019-07-20 14:46:50 +0300

    Updates

---
+++ precache-manifest.053eada6ad9bea258d59252df152f794.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a5db2a4fd6ea558ed822ed3afeeff0",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "6758fa83e0c3295671bc",
+    "url": "/react_people-table/static/css/main.fbcfa6e5.chunk.css"
+  },
+  {
+    "revision": "9ed6223c153fe67fe18c",
+    "url": "/react_people-table/static/js/2.97a7f758.chunk.js"
+  },
+  {
+    "revision": "6758fa83e0c3295671bc",
+    "url": "/react_people-table/static/js/main.e8b86440.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
